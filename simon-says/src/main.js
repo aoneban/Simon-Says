3a0a167f585ce37b3853a294fe5940d58497480a
aoneban <torgregion1@gmail.com>
2025-01-10 15:24:34 +0100

    feat: create keyboard and input

--- simon-says/src/main.js
+++ simon-says/src/main.js
@@ -1,14 +1,68 @@
 import './styles.scss';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <h1 class="text-4xl font-bold">Hello Vite!</h1>
-  </div>
-`;
+const buttonNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const buttonLetters = [
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+];
 
-setupCounter(document.querySelector('#counter'));
+class Keyboard {
+  constructor(arr) {
+    this.arr = arr;
+    this.keyboardOne = null;
+    this.keyboardTwo = null;
+  }
+
+  createWrapperApp() {
+    const bodyElement = document.getElementById('app');
+
+    const input = document.createElement('input');
+    input.classList.add('text-input');
+    bodyElement.append(input);
+
+    const keyboardWrapper = document.createElement('div');
+    keyboardWrapper.classList.add('keyboard-wrapper');
+    bodyElement.append(keyboardWrapper);
+
+    this.keyboardOne = document.createElement('div');
+    this.keyboardOne.classList.add('keyboard');
+    keyboardWrapper.append(this.keyboardOne);
+
+    this.keyboardTwo = document.createElement('div');
+    this.keyboardTwo.classList.add('keyboard-2');
+    keyboardWrapper.append(this.keyboardTwo);
+
+    const paragraph1 = document.createElement('p');
+    paragraph1.append('Клавиатура создана в операционной системе iOS');
+    bodyElement.append(paragraph1);
+  }
+
+  createKeyboardNumbers(arr) {
+    arr.map((item) => {
+      const button = document.createElement('button');
+      button.classList.add('button');
+      button.append(item);
+      this.keyboardOne.append(button);
+    });
+  }
+
+  createKeyboardLetters(arr) {
+    for (let i = 0; i < arr.length; i += 1) {
+      const block = document.createElement('div');
+      block.classList.add('block');
+      arr[i].map((item) => {
+        const button = document.createElement('button');
+        button.classList.add('button');
+        button.append(item);
+        block.append(button);
+      });
+      this.keyboardTwo.append(block);
+    }
+  }
+}
+
+const keyboard = new Keyboard();
+keyboard.createWrapperApp();
+keyboard.createKeyboardNumbers(buttonNumbers);
+keyboard.createKeyboardLetters(buttonLetters);
